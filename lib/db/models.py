7733286e32c145cb4f93f4a1ed9e4df4f821a184
Sterling <sterlingbarton81@gmail.com>
2023-05-15 15:21:04 -0600

    created initial tables

---
+++ lib/db/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine('sqlite:///maze.db')
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+
+    game = relationship('Game', back_populates='user')
+
+
+class Maze(Base):
+    __tablename__ = 'mazes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    difficulty = Column(String)
+
+    game = relationship('Game', back_populates='maze')
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    maze_id = Column(Integer, ForeignKey('mazes.id'))
+
+    user = relationship('User', back_populates='game')
+    maze = relationship('Maze', back_populates='game')
